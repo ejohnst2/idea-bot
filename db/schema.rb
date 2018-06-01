5d982a7f1fd415825a5d4ee1d0250c83746de1b7
Nick Sarafa <nicksarafa@gmail.com>
2018-06-01 17:01:44 -0600

    remove meals table from db schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,15 +24,6 @@ ActiveRecord::Schema.define(version: 2018_03_22_075558) do
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
-  create_table "meals", force: :cascade do |t|
-    t.string "name"
-    t.text "image_data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_meals_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "telegram_id"
     t.string "username"
