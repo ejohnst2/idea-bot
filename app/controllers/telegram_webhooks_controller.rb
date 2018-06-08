71a9781ce38e4d01c3c2ae55ed114e0d775beb3a
Nick Sarafa <nicksarafa@gmail.com>
2018-06-08 12:06:30 -0500

    share_new_idea method init

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -85,12 +85,14 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     user.ideas.create name: payload["caption"],
                       image_remote_url: photo_url,
                       created_at: payload_timestamp
+    share_new_idea
   end
 
+  # shares new idea to the Telegram group
   def share_new_idea
     bot.send_message(
       chat_id: ENV.fetch("TELEGRAM_GROUP_ID"),
-      text: %(@#{user.username} created a new idea!)
+      text: %(💡 @#{user.username} created a new idea!)
     )
   end
 
