8c3379263eb73ea64904f1bf0c2a7c9810f4dac9
elijah johnston <eli@millinno.com>
2018-08-10 13:49:48 +0800

    landing page init

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :charges
 
+  get '/index', to: 'pages#index'
   get '/landing', to: 'pages#landing'
   root to: "pages#home"
 end
