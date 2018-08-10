ddfe8679db434735e4a0a510f951e9c9765d8b2f
elijah johnston <eli@millinno.com>
2018-08-10 09:10:33 +0800

    removed commented out methods in model

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,12 +1,2 @@
 class Charge < ApplicationRecord
-  # after_create :add_to_array
-
-  # def self.all
-  #   # Lazy-initialize the collection to an empty array
-  #   @all ||= [ ]
-  # end
-
-  # def add_to_array
-  #   Charge.all << self.email
-  # end
 end
